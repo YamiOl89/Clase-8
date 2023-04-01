a261fd737ba4da2961c4f7e91987d3fb27e1fd7f
50233 <50233@DESKTOP-5OVIGDK>
2023-04-01 08:54:16 -0600

    Agregar archivos de proyecto.

---
+++ Clase 8/Program.cs
@@ -0,0 +1,33 @@
+﻿void arreglos()
+{
+    string[] nombre = new string[]
+    {
+"juan","maria","pedro"
+
+    };
+
+foreach (string n in nombre){
+        Console.WriteLine(n);
+    }
+
+    int[] notas = new int[] {
+
+
+50,30,90
+};
+    int promedio = 0;
+    foreach (int i in notas) {
+        promedio = promedio + i;
+    }
+    promedio=promedio/notas.Length;
+    
+    Console.WriteLine($"El promedio es: {promedio}");
+
+
+
+
+
+
+
+}
+arreglos();
---
+++ ejercicio en clase 8/Program.cs
@@ -0,0 +1,26 @@
+﻿void arreglos()
+{
+    String[] nombre = new string[]
+    {
+ "Fernanda", "Melvin", "Luis", "Brandon", "Yamileth"
+    };
+
+    int[] notas = new int[]
+    {
+    70,90,43,85,30
+    };
+    int notaMasAlta = notas[0];
+    foreach (int nota in notas)
+    {
+        if (nota > notaMasAlta)
+        {
+            notaMasAlta = nota;
+        }
+    }
+    Console.WriteLine("La nota más alta es: "  + notaMasAlta);
+};
+
+arreglos();
+
+
+
